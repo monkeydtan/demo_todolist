035440deed8b054c4040bbcbda1ae4257652445b
Monkeydtan <gamger12@gmail.com>
2024-09-20 16:08:44 +0700

    modify more, add event and funcion for saving todolist 2

--- todo_page.py
+++ todo_page.py
@@ -1,22 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
 from PIL import Image,ImageTk
+from tkinter import messagebox
 
 app = tk.Tk()
 app.title("To Do List")
 app.geometry("500x500")
 app.configure(background="light steel blue")
 
-list_count = 1 #ตัวแปรเริ่มต้นที่ 1
+#list_count = 1 #ตัวแปรเริ่มต้นที่ 1
 text_placeholder = "เพิ่มรายการของคุณ" # ข้อความเริ่มต้นที่จะแสดงที่ช่องกรอกข้อความ (message_box)
 
 # ฟังก์ชันการกด Add แล้วมีการบันทึกลงใน Listbox
 def msg_todolist():
-    global list_count
+    #global list_count
+    global selected_list
     msg = message_box.get("1.0", tk.END).strip() #ดึงข้อความจากช่องกรอกข้อความ และ เก็บไว้ในตัวแปร msg
     if msg and msg != text_placeholder:
-        list_box.insert(tk.END,f"{list_count}. {msg}")
-        list_count += 1
+        #list_box.insert(tk.END,f"{list_count}. {msg}")
+        #list_count += 1
+        list_box.insert(tk.END,msg)
         message_box.delete("1.0",tk.END) # ก่อนหน้าใช้ message_box.delete(0,tk.END) มันเลยไม่กลับไปเป็นค่าเริ่มต้น
         add_placeholder()
         
@@ -35,7 +38,16 @@ def clear_placeholder(event=None):
         message_box.delete("1.0",tk.END) # ลบ
         message_box.config(fg="black")
         
-        
+# ฟังก์ชันการลบ List ที่เคยบันทึกไว้
+def delete_list(event):
+    deleted_list = list_box.curselection() #.curselection()การเลือก เท่ากับว่า เลือก list box และเก็บค่าไว้ในตัวแปร deleted_list 
+    if deleted_list:
+        item_list = list_box.get(deleted_list)
+        confirm = messagebox.askyesno("ลบรายการ","คุณต้องการลบรายการนี้ใช่หรือไม่")
+        if confirm:
+            list_box.delete(deleted_list)
+
+    
 # กรอบหัวข้อ
 frame_header = tk.Frame(app,background='light steel blue')
 frame_header.pack(expand=False)
@@ -56,6 +68,7 @@ message_box.bind("<FocusIn>",clear_placeholder)
 message_box.bind("<FocusOut>",add_placeholder)
 message_box.bind("<Key>",clear_placeholder)
 
+
 add_btn = tk.Button(frame_header,width=5,text="Add",command=msg_todolist)
 add_btn.grid(row=2,column=0,pady=(10,0),sticky="e")
 
@@ -65,6 +78,9 @@ header_listbok.grid(row=3,column=0,pady=(10,0),sticky="w")
 # list bok
 list_box = tk.Listbox(frame_header)
 list_box.grid(row=4,column=0,pady=(5,0),sticky="nsew")
+
+# เชื่อมเหตุการณ์(event) กับ ฟังก์ชัน การลบ List
+list_box.bind("<Double-1>",delete_list)
 
 
 
